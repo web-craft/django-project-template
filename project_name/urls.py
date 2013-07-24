8a97a6e230319c8a3a230efd2d465ab318c185ab
Anton Smirnov <smirnoff.anton@gmail.com>
2013-07-25 01:27:45 +0400

    enable admin by default

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,22 +1,14 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', direct_to_template, {'template': 'index.html'}),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
