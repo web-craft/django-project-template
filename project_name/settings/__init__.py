e1473db61b0c2aa101caa1d38d4674c33ec669c4
Anton Smirnov <smirnoff.anton@gmail.com>
2013-07-25 00:33:57 +0400

    move settings

--- project_name/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-try:
-    from .local import *
-except ImportError, e:
-    e.args = tuple(['%s (did you copy settings/local.py.template to settings/local.py?)' % e.args[0]])
-    raise e
---
+++ settings/__init__.py
@@ -0,0 +1,6 @@
+from .settings.base import *
+try:
+    from .local import *
+except ImportError, e:
+    e.args = tuple(['%s (did you copy settings/local.py.template to settings/local.py?)' % e.args[0]])
+    raise e
